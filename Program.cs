abecd4de65384b1ff6f8f04334869a68ac8b5ac9
Pedro Padilha Neto <pedropadilhan@gmail.com>
2023-03-09 00:50:18 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace HorasEmDias
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Informe a quantidade de horas: ");
+            int horas = int.Parse(Console.ReadLine());
+            int dias = horas / 24;
+            Console.WriteLine($"A quantidade de dias é: " + dias);
+
+        }
+    }
+}
